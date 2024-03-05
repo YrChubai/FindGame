d7c672fdb9bd0433b55cd70f8e4dd36077350617
YrChubai <razawchs@gmail.com>
2024-03-05 13:04:02 +0300

    Add NavController

--- app/src/main/java/com/example/findgame/MainActivity.kt
+++ app/src/main/java/com/example/findgame/MainActivity.kt
@@ -10,37 +10,33 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.findgame.ui.theme.FindGameTheme
+import com.example.findgame.view.EndGamePopup
+import com.example.findgame.view.GameScene
+import com.example.findgame.view.MenuView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FindGameTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            val navController = rememberNavController()
+            NavHost(
+                navController = navController,
+                startDestination = "route_screen")  {
+                composable("route_screen") {
+                    MenuView(navController)
+                }
+                composable("game") {
+                    GameScene(navController)
+                }
+                composable("end") {
+                    EndGamePopup(navController)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FindGameTheme {
-        Greeting("Android")
-    }
-}
