eb72cdbf2f84979167674323ec4e5a753098c696
YrChubai <razawchs@gmail.com>
2024-03-05 13:02:22 +0300

    Add MenuView

---
+++ app/src/main/java/com/example/findgame/view/MenuView.kt
@@ -0,0 +1,43 @@
+package com.example.findgame.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.findgame.GameSceneVM
+import androidx.lifecycle.viewmodel.compose.viewModel
+@Composable
+fun MenuView(navController: NavController, viewModel: GameSceneVM = viewModel()
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(White)
+                .padding(16.dp)
+        ) {
+            Button(onClick = { navController.navigate("game") }) {
+            }
+        }
+    }
+}
